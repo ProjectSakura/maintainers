7e580746f31505d3e652d72910f34033ac1711ee
ArunTeltia <teltia.arun18@gmail.com>
2021-04-18 16:35:21 +0530

    deploying website

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Pages Import
-import Contributors from "./components/contributors/contributors"
+import Contributors from "./components/contributors/contributors";
 
 function App() {
   return (
     <header className="App-header">
       <Router>
         <Switch>
-          <Route exact path="/" component={Contributors} />
+          <Route exact path="/contributors" component={Contributors} />
         </Switch>
       </Router>
     </header>
